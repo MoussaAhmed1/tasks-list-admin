b5318d8e4eb624a4d473f00a5f559f291dae7c3c
Mousa Ahmed <moussa.abdelghany@gmail.com>
2024-10-08 23:49:34 +0300

    nav-bar

--- src/app/dashboard/layout/layout.component.ts
+++ src/app/dashboard/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
+  lang:any = "en"
+  constructor( private router:Router) { 
+   
+  }
 
+  ngOnInit(): void {
+  }
+
+  changeLanguage() {
+    if(this.lang == "en") {
+      localStorage.setItem('language' , 'ar')
+    } else {
+      localStorage.setItem('language' , 'en')
+    }
+    window.location.reload()
+  }
+
+  logout() {
+    this.router.navigate(['/auth'])
+    localStorage.removeItem('token')
+  }
 }
